0749758182dcd86e9547be2d0b7012618f99287f
Lluis Llull <lluisllullriera@gmail.com>
2023-03-21 22:03:44 +0100

    second commit

--- Graf.cc
+++ Graf.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <queue>
 
 using namespace std;
 
@@ -33,7 +34,29 @@ class Graph {
                 }
                 cout << endl;
             }
+        }
 
+        vector<bool> simulateDifusion( vector<int> S) {
+            int n = adj_list.size();
+            int randomNumber, visiting;
+            int countVisited = 0;
+
+            vector<bool> visited (n, false);
+            queue<int> toVisit (deque<int>(S.begin(), S.end()));
+
+            while(countVisited < n and not toVisit.empty()){
+
+                visiting = toVisit.front(); 
+                toVisit.pop();
+                visited[visiting] = true;
+                for(int i = 0; i < adj_list[visiting].size(); i++) {
+                    randomNumber = int(rand() % 2);
+                    if(randomNumber == 1 and not visited[i]) {
+                        toVisit.push(i);
+                    }
+                }
+            }
+            return visited;
         }
 };
 
@@ -45,8 +68,12 @@ int main() {
         {1, 3},
         {1, 4}
     };
-
+    vector<int> S {1,2};
     G.defineGraph(4, vertexs, 0.5);
-    G.showGraph();
+    //G.showGraph();
+    vector<bool> as = G.simulateDifusion(S);
+    for(int k = 0; k< as.size();++k) {
+        cout<<as[k]<<endl;
+    }
     cout << "FIN"<<endl;
 }
