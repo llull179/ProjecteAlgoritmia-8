71a66d3ac42ec028bbf9742be735620335924368
ivan-serrano-hernandez <i.serranoher@gmail.com>
2023-03-29 00:08:27 +0200

    Algorisme greedy casi acabat, falten corregir alguns errors i fer tests

--- Algorithms/IC/Greedy.hh
+++ Algorithms/IC/Greedy.hh
@@ -1,29 +1,25 @@
 #include "IC_difussionGraph.hh"
-#include <queue>
-#include <vector>
+#include <cmath>
+#include <list>
+
+
+typedef pair<int,int> ppair;
+
 
 class Greedy: private IC_difussionGraph{
 
-    private:
-        // <degree, vertexId>
-        priority_queue <pair<int,int>> degree;
-        
     public:
-
 
         Greedy(int n){
             this-> n = n;
             g.resize(n);
             cout << g.size() << endl;
-            // O(n)
             spreadedNodes.resize(n, false);
-
         }
 
-        void readEdges(int m){
+        void readEdges(int m, double p){
             this-> m = m;
 
-            // O(n)
             for(int i = 0; i < m; i++){
                 int a, b;
                 cin >> a >> b;
@@ -31,62 +27,74 @@ class Greedy: private IC_difussionGraph{
                 g[a].push_back(b);
                 g[b].push_back(a);
             }
+            this->p = p;     
+        }
 
-            // fills the priority_queue -> O(n*lgn)
-            for(int i = 0; i < n; i++){
-                degree.push(make_pair(g[i].size(), i));
+        void beginDifusion(){
+            // initially no nodes propagated
+            int propagatedNodes = 0;
+            list <int> subset;
+            vector<bool> inSubset(n, false);
+
+            while(propagatedNodes != this->n){
+                // pick wich node to propagate
+                double maxInfluence = 0.0;
+                int idx = 0;
+                for(int i = 0; i < this->n; i++){
+                    // pick node not propagated yet
+                    if(not inSubset[i]){
+                        double nodeInfluence = computeNodeInfluence(i);
+                        if(nodeInfluence > maxInfluence){
+                            maxInfluence = nodeInfluence;
+                            idx = i;
+                        }
+                    }
+                }
+                // add node to subset
+                inSubset[idx] = true;
+                subset.push_back(idx);
+                readStartingSubset(subset);
+                propagatedNodes = propagate();
             }
         }
 
+        double computeNodeInfluence(int src){
+            // Priority queue for vertices that are being processed
+            queue <int> Q;
+            Q.push(src);
 
-        void beginDifusion(){
-            int spreaded = 0;
-            double maxInfluence = 0.0;
-            int maxInfluenceNode = 0;
+            // Vector for disntances
+            vector <int> distances(this->n, __INT_MAX__);
+            distances[src] = 0;
 
-            // compute influence of all nodes
-            while(spreaded != n){
-                for(int i = 0; i < this->n; i++){
-                    if (not spreadedNodes[i]){
-                        double tmpInfluence = computeGraphInfluence(i);
+            // Vector for visited nodes
+            vector <bool> visited(n, false);
+            visited[src] = true;
+
+            while(not Q.empty()){
+                // next node
+                int u = Q.front();
+                Q.pop();
+                // visit all neightbours
+                for(int i = 0; i < g[u].size(); i++){
+                    // next neightbour
+                    int v = g[u][i];
+                    // if not visited, mark as visited and enqueue
+                    if(not visited[v]){
+                        distances[v] = distances[u] + 1;
+                        visited[v] = true;
+                        Q.push(v);
                     }
                 }
             }
-        }
+            
 
-        /*
-        Given a node v, computes influence of u in the graph as the sum
-        of influence to every other node in the graph */
-        double computeGraphInfluence(int u){
-
-            // distances array
-            vector <int> distances = vector<int>(n, __INT_MAX__);
-            // visited nodes
-            vector <bool> visited = vector<bool>(n, false);
-
-            // source node metrics
-            double nodeInfluence = 0.0;
-            distances[u] = 0;
-            visited[u] = true;
-
-            for(int i = 0; i < this-> n; i++){
-                
-
-                // checks if node has been spreaded and ignore it if so
-                if(not spreadedNodes[i]){
-                    
-                }
+            // compute global influence as the sum of all influences
+            double globalInfluence = 0.0;
+            for(int i = 0; i < n; i++){
+                globalInfluence += pow(this->p,distances[i]);
             }
+            return globalInfluence;
         }
 
-        int minDistance(vector<int>& distances, vector<bool>& visited){
-            int minimum=__INT_MAX__, idx; 
-            for(int i = 0; i < this-> n; i++){
-                if(not visited[i] && distances[i]<=minimum){
-                    minimum = distances[i];
-                    idx=i;
-                }
-            }
-            return idx;
-        }
-};  
+};
