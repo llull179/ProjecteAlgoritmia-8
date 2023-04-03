74c09618fe1d597b77458e06a3e65636d3d248d2
mrec8 <mattinrecaldetarron@gmail.com>
2023-04-03 18:42:01 +0200

    duplicate greedy functions

--- LT/LT_Greedy.hh
+++ LT/LT_Greedy.hh
@@ -59,7 +59,7 @@ class LTGreedy: private LT_difussionGraph {
             file.close();
         }
 
-        void beginDifusion(){
+        void beginDifusionLTInit(){
             // initially no nodes propagated
             int propagatedNodes = 0;
             list<int> subset;
@@ -72,7 +72,7 @@ class LTGreedy: private LT_difussionGraph {
             auto begin = std::chrono::high_resolution_clock::now();
             priority_queue < ppair, vector<ppair>, less<ppair> > Q;
             for (int i = 0; i < this -> n; ++i) {
-                int p = computeNodeInfluence(i);
+                int p = computeNodeInfluence(i,inSubset);
                 Q.push(pair(p,i));
             }
 
@@ -116,7 +116,67 @@ class LTGreedy: private LT_difussionGraph {
             cout << "Difusion ended, check output-LT-difusion file to see benchmarks and the result" << endl;
         }
 
-        double computeNodeInfluence(int src){
+        void beginDifusionLTAlways(){
+            // initially no nodes propagated
+            int propagatedNodes = 0;
+            list<int> subset;
+            vector<bool> inSubset(n, false);
+            // Prints is redirected to a file
+            ofstream file;
+            file.open("output-LT-difusion");   
+
+            // sets timer
+            auto begin = std::chrono::high_resolution_clock::now();
+            priority_queue < ppair, vector<ppair>, less<ppair> > Q;
+            
+
+            int iteration = 0;
+            list<int> newSubset = subset;
+            int newPropagatedNodes = propagatedNodes;
+            while(propagatedNodes != this->n){
+                int max = 0;
+                int idx = -1;
+                for (int i = 0; i < this -> n; ++i) {
+                    int p = computeNodeInfluence(i,inSubset);
+                    if (p > max) {
+                        max = p;
+                        idx = i;
+                    }
+                }
+                // add node to subset
+                if (not inStartingSubset(idx)) {
+                    inSubset[idx] = true;
+                    subset.push_back(idx);
+                    list<int> l;
+                    l.push_back(idx);
+                    modStartingSubset(l);
+                    newPropagatedNodes = propagate(); 
+                    if (newPropagatedNodes == propagatedNodes) {
+                        subset.pop_back();
+                        //newSubset.pop_back();
+                    }
+                    else propagatedNodes = newPropagatedNodes;
+                    iteration++;
+
+                    // output to file current subset
+                    file << "Iteration " << iteration << ", current subset of nodes:";
+                    list<int>::const_iterator it=subset.begin();
+                    while(it != subset.end()){
+                        file << " " << (*it);
+                        it++;
+                    }file << endl << "--------------------" << endl << endl;
+                }
+            }                
+            // stop elapsed timer
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+            file << "Difusion completed in " << iteration << " steps, " << elapsed.count() * 1e-9 << "s." << endl;
+            file.close();
+
+            cout << "Difusion ended, check output-LT-difusion file to see benchmarks and the result" << endl;
+        }
+
+        double computeNodeInfluence(int src, vector<bool> &S){
             // Priority queue for vertices that are being processed
             priority_queue < ppair, vector<ppair>, greater<ppair> > Q;
             Q.push(pair(0,src));
@@ -140,7 +200,7 @@ class LTGreedy: private LT_difussionGraph {
                         int v = g[u][i];
                         // modify distance if necesssary
                         double s = g[v].size();
-                        if(distances[v] < distances[u]+distances[u]*1/s){
+                        if(not S[v] and distances[v] < distances[u]+distances[u]*1/s){
                             distances[v] = distances[u] + distances[u]*1/s;
                             visited[v] = true;
                             Q.push(pair(-distances[v], v));
