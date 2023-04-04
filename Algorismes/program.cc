00d92b3419c80777e53805e2430d03855452776b
llull179 <lluisllullriera@gmail.com>
2023-04-04 17:38:07 +0200

    sol program

--- Algorismes/program.cc
+++ Algorismes/program.cc
@@ -1,9 +1,6 @@
-
 #include "Greedy.hh"
 #include "LocalSearch.hh"
-
 #include <string>
-
 #define INPUT_PATH "../Utilities/input-graphs/"
 
 
@@ -22,58 +19,51 @@ int main(int argc, char * argv[]){
         return -1;
     }
 
-    //Intoduce propability for IC and wieght for LT
-    double pr;
-    if(dmode == "IC") cout << "Introduce Spreading probability: ";
-        else  cout << "Introduce Spreading ratio: ";
-        cin >> pr; cout << endl;
 
 
-    //Introduce which algorith execute
-    int action;
-    cout << "Select action:" << endl;
-    cout << "Type 0 for just difusion" << endl;
-    cout << "Type 1 for Greedy Algorithm" << endl;
-    cout << "Type 2 for LocalSearch difusion" << endl;
-    cout << "Type 3 for Metauristic Algorithm" << endl;
-    cin >> action;
-
-    //declarar el tipo de algoritmo
-    LocalSearch g = LocalSearch();
-    
     // manual graph input
     if(argc == 1){
-        int n, m; 
+        int n, m; double pr;
         cout << "Number of nodes: " ;
         cin >> n;
+        Greedy g = Greedy(n);
         cout << "Number of edges: " ;
         cin >> m;
 
+        if(dmode == "IC") cout << "Introduce Spreading probability: ";
+        else  cout << "Introduce Spreading ratio: ";
+        cin >> pr; cout << endl;
+
         cout << "Introduce edges in the folllowing format : i j " << endl;
         g.readEdges(m, pr);
-          
+
+        // begin difusion
+        if(dmode == "IC")  g.beginDifusion_IC(); 
+        else ;
+
     }
+
     // graph input from file
     else if (argc == 2){
         string filename = argv[1];
+        LocalSearch g = LocalSearch();
+        double pr;
+
+        if(dmode == "IC") cout << "Introduce Spreading probability: ";
+        else cout << "Introduce Spreading ratio: ";
+        cin >> pr; cout << endl;
 
         g.readEdgesFromFile(pr, INPUT_PATH + filename);
-  
+
+        // begin difusion
+        if(dmode == "IC")  g.beginDifusion(true,1); 
+        else ;   
     }
     else{
         cerr << "Invalid number of arguments." << endl;
         return -1;
     }
 
-    /*if(action ==1) {
-        if(dmode == "IC")g.beginDifusion_IC();
-        else LT difusion;
-    }
-    else if(action == 2) {*/
-        if(dmode=="IC") g.beginDifusion(true);
-        else g.beginDifusion(false);
-    //}
     return 0;
 
 }
-
