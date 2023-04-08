69f9ce8cafc326f25a60e268f236e0aa9cdda59c
mimugara <miquelmunozz@gmail.com>
2023-04-08 18:42:35 +0200

    "SA funciona SUUUUUU"

--- Algorismes/SA.cc
+++ Algorismes/SA.cc
@@ -13,7 +13,7 @@ int main(int argc, char * argv[]){
         cin >> n;
         cout << "Number of vertices: " ;
         cin >> m;
-        cout << "Introduce Spreading ratio: ";
+        cout << "Introduce Spreading ratio or Spreading probability: ";
         cin >> r; cout << endl;
         SA G = SA(n,r);
         cout << "Introduce edges in the folllowing format : i j " << endl;
@@ -21,7 +21,22 @@ int main(int argc, char * argv[]){
         vector<bool> sol_ini = G.getMinDominantSet();
         //definim la temperatura
         int temp = 100;
-        G.simulated_annealing(sol_ini,temp);
+            // ask user for difussion model
+        cout << "Select difusion model:" << endl;
+        cout << "Type <IC> for independent cascade, or <LT> for Linear Threshold" << endl;
+        string dmode;
+        cin >> dmode;
+
+        if (dmode != "LT" && dmode != "IC"){
+            //invalid mode
+            cerr << "Invalid difusion model" << endl;
+            return -1;
+        }
+        bool modeIC;
+        if (dmode == "IC") modeIC = true;
+        else modeIC = false;
+        double varEnergia = 50; //la variacio de lenergia sempre es la mateixa ja que afegim o treiem un node
+        G.simulated_annealing(sol_ini,temp,modeIC,varEnergia);
 
     }
     // graph input from file
@@ -30,18 +45,31 @@ int main(int argc, char * argv[]){
         string filename = argv[1];
         SA G = SA();
         
-        cout << "Introduce Spreading probability: ";
+        cout << "Introduce Spreading ratio or Spreading probability: ";
         double r; cin >> r; cout << endl;
         
 
         G.readEdgesFromFile(r, INPUT_PATH + filename);
 
        vector<bool> sol_ini = G.getMinDominantSet();
-        cout << "hola" << endl;
         //definim la temperatura
         int temp = 100;
- 
-        G.simulated_annealing(sol_ini,temp);
+     // ask user for difussion model
+        cout << "Select difusion model:" << endl;
+        cout << "Type <IC> for independent cascade, or <LT> for Linear Threshold" << endl;
+        string dmode;
+        cin >> dmode;
+
+        if (dmode != "LT" && dmode != "IC"){
+            //invalid mode
+            cerr << "Invalid difusion model" << endl;
+            return -1;
+        }
+        bool modeIC;
+        if (dmode == "IC") modeIC = true;
+        else modeIC = false;
+        double varEnergia = 50; //la variacio de lenergia sempre es la mateixa ja que afegim o treiem un node
+        G.simulated_annealing(sol_ini,temp,modeIC,varEnergia);
     }
     else{
         cerr << "Invalid number of arguments." << endl;
--- Algorismes/SA.hh
+++ Algorismes/SA.hh
@@ -27,37 +27,35 @@ class SA: public difussionGraph {
 
         // public methods --------------------------------------------------
 
-        void simulated_annealing(const vector<bool> & ini, int temp) {
+        void simulated_annealing(const vector<bool> & ini, int temp, bool IC, double varEn) {
             int r;
             vector<bool> sol = ini;
             vector<int> espai_solucions;
             while(temp > 0) {
-                for (int i = 0; i < 10; ++i) {   
+                for (int i = 0; i < 100; ++i) {   
                     espai_solucions.clear();
                     for(int i = 0; i < sol.size(); ++i) {
                         if (sol[i]) {
                             sol[i] = false;
-                            if(isSolution(sol,false)) espai_solucions.push_back(i);
+                            if(isSolution(sol,IC))  espai_solucions.push_back(i);                     
                             sol[i] = true;
                         }
                         else espai_solucions.push_back(i);
                     }             
                     r = rand()%espai_solucions.size();
                     //forma part de S 
-                    if (sol[r]) sol[r] = false;  
+                    if (sol[espai_solucions[r]]) sol[espai_solucions[r]] = false;  
                     //no forma part de S (veiem si el podem afegir)
                     else {
-                        double prob = pow(10, -1/temp);
-                        int bound = rand()%100;
-                        if (prob*100 <= bound) sol[r] = true;
+                        double prob = pow(10.0, -varEn/temp);
+                        cout << prob << endl;
+                        int al = rand()%100;
+                        if (al <= prob*100) sol[r] = true;
                     }
-                    cout << temp << endl;
                 }
       
                 --temp;
-            }
-             cout << "he acabado" << endl;
-             
+            }             
              for(int i = 0; i < sol.size(); ++i) {
                 if (sol[i]) cout << i << endl;
              }
